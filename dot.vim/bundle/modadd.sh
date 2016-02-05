84ee8de77cf1fe4f4b18e88ef58ebcc519aa2ec8
Mark Pitchless <markpitchless@gmail.com>
2016-02-05 21:45:11 +0000

    Add pathogens as sub modules

---
+++ dot.vim/bundle/modadd.sh
@@ -0,0 +1,14 @@
+git submodule add https://github.com/scrooloose/nerdtree.git
+git submodule add https://github.com/scrooloose/nerdcommenter.git
+git submodule add https://github.com/mileszs/ack.vim.git ack
+git submodule add git://github.com/godlygeek/tabular.git
+git submodule add git://github.com/chase/vim-ansible-yaml.git
+git submodule add git clone https://github.com/fatih/vim-go.git
+git submodule add https://github.com/elzr/vim-json
+git submodule add https://github.com/naegelejd/vim-swig
+git submodule add https://github.com/bling/vim-airline
+git submodule add git://github.com/airblade/vim-gitgutter.git
+git submodule add git://github.com/tpope/vim-fugitive.git
+git submodule add git://github.com/altercatio/vim-colors-solarized.git
+git submodule add https://github.com/majutsushi/tagbar
+git submodule add https://github.com/ervandew/supertab
